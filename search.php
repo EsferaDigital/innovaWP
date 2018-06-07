68c14e522e26b452e9c662252acb91637eeafd40
Gabriel Zavando <gabrielzavando@gmail.com>
2018-06-06 20:27:34 -0400

    termine la pagina search

--- search.php
+++ search.php
@@ -1,9 +1,17 @@
+<style>
+.c-header{
+	position:static !important;
+	background-color: #0075a2;
+}
+</style>
 <?php
 get_header();
-$search_template = '
-	<p>Los resultados para la busqueda <span>%s</span> son:</p>
-';
-printf($search_template, get_search_query() );
-get_template_part('/plantillas/content');
-get_sidebar();
+printf('<section class="container-central">');
+	$search_template = '
+		<p class="resultado-search">Los resultados para la busqueda <span>"%s"</span> son:</p>
+	';
+	printf($search_template, get_search_query() );
+	get_template_part('/plantillas/content');
+	get_sidebar();
+printf('</section>');
 get_footer();
