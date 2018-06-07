558036b9d48684c5c8a576d0f1bc6464499ef013
Gabriel Zavando <gabrielzavando@gmail.com>
2018-06-06 20:45:34 -0400

    termine la pagina archive

--- archive.php
+++ archive.php
@@ -1,6 +1,13 @@
+<style>
+.c-header{
+	position:static !important;
+	background-color: #0075a2;
+}
+</style>
 <?php
 get_header();
-printf('<h1>Soy el archive</h1>');
-get_template_part('/plantillas/content');
-get_sidebar();
+printf('<section class="container-central">');
+	get_template_part('/plantillas/content');
+	get_sidebar();
+printf('</section>');
 get_footer();
